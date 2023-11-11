f58db435f286214072faf3b649ff1feab51e573c
Josh <jyeria01@gmail.com>
2023-11-11 08:42:37 -0500

    Exercise 2 Final Commit

--- manage_employees.py
+++ manage_employees.py
@@ -46,3 +46,27 @@ class ProductionWorker(Employee):
     # Accessor method for getting the employee's pay_rate
     def get_pay_rate(self):
         return self.__pay_rate
+    
+class ShiftSupervisor(Employee):
+
+    # Initialize the ShiftSupervisor object
+    def __init__(self, name, id_number, annual_salary, annual_bonus):
+        Employee.__init__(self, name, id_number)
+        self.__annual_salary = annual_salary
+        self.__annual_bonus = annual_bonus
+
+    # Mutator Method for annual salary
+    def set_annual_salary(self, annual_salary):
+        self.__annual_salary = annual_salary
+
+    # Mutator method for annual bonus
+    def set_annual_bonus(self, annual_bonus):
+        self.__annual_bonus = annual_bonus
+
+    # Accessor method for annual salary
+    def get_annual_salary(self):
+        return self.__annual_salary
+
+    # Accessor method for annual bonus
+    def get_annual_bonus(self):
+        return self.__annual_bonus
